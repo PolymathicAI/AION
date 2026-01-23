6083f08fdbfd4f718068a6e52d1d19f5800de2bd
Jeff Shen <jshen2014@hotmail.com>
2026-01-23 11:43:56 -0500

    make padding operate on Spectrum class; apply padding in codec

--- aion/codecs/preprocessing/spectrum.py
+++ aion/codecs/preprocessing/spectrum.py
@@ -1,20 +1,17 @@
-import torch
 import torch.nn.functional as F
 
+from aion.modalities import Spectrum
 
-def pad_spectrum(sample, max_length: int = 4800):
+
+def pad_spectrum(x: Spectrum):
     padding_values = {"lambda": 99999, "mask": True, "ivar": 0}
 
-    for k in sample["spectrum"].keys():
-        if (
-            isinstance(sample["spectrum"][k], torch.Tensor)
-            and sample["spectrum"][k].ndim == 1
-        ):
-            sample["spectrum"][k] = F.pad(
-                sample["spectrum"][k],
-                (0, max_length - len(sample["spectrum"][k])),
-                mode="constant",
-                value=padding_values[k] if k in padding_values else 0,
-            )
+    for k in ["flux", "ivar", "mask", "wavelength"]:
+        x.k = F.pad(
+            x.k,
+            (0, x.pad_length - len(x.k)),
+            mode="constant",
+            value=padding_values[k] if k in padding_values else 0,
+        )
 
-    return sample
+    return x
