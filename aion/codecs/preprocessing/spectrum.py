69edd6d45ccd1e3a112d4dc8bda2ff19fa9431ff
Jeff Shen <jshen2014@hotmail.com>
2026-01-23 11:40:30 -0500

    preprocessing in torch

--- aion/codecs/preprocessing/spectrum.py
+++ aion/codecs/preprocessing/spectrum.py
@@ -1,14 +1,20 @@
-import numpy as np
+import torch
+import torch.nn.functional as F
 
 
 def pad_spectrum(sample, max_length: int = 4800):
     padding_values = {"lambda": 99999, "mask": True, "ivar": 0}
 
     for k in sample["spectrum"].keys():
-        sample["spectrum"][k] = np.pad(
-            sample["spectrum"][k],
-            (0, max_length - len(sample["spectrum"][k])),
-            constant_values=padding_values[k] if k in padding_values else 0,
-        )
+        if (
+            isinstance(sample["spectrum"][k], torch.Tensor)
+            and sample["spectrum"][k].ndim == 1
+        ):
+            sample["spectrum"][k] = F.pad(
+                sample["spectrum"][k],
+                (0, max_length - len(sample["spectrum"][k])),
+                mode="constant",
+                value=padding_values[k] if k in padding_values else 0,
+            )
 
     return sample
